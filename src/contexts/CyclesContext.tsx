2c75a58b869f5c58ba68683e97f737fc336a8db1
walacefex <walacefex01@gmail.com>
2023-10-03 01:04:47 -0300

    create new function to called context and reset

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -67,7 +67,6 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(newCycle.id)
     setAmountSecondsPassed(0)
-    // reset();
   }
 
   function interruptCurrentCycle() {    
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -36,14 +36,19 @@ export function Home() {
     }
   })
   
-  const  { handleSubmit, watch } = newCycleForm;
+  const  { handleSubmit, watch, reset } = newCycleForm;
+
+  function handleCreatreNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task;
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)} action="">
+      <form onSubmit={handleSubmit(handleCreatreNewCycle)} action="">
       
           <FormProvider {...newCycleForm}>
             <NewCycleForm />
