4f223235c047587f987fff77a91cdccd891ce613
walacefex <walacefex01@gmail.com>
2023-10-03 01:32:45 -0300

    include formated in date list

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from "date-fns"
+import ptBR from 'date-fns/locale/pt-BR'
 import { useContext } from 'react'
 import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
@@ -24,7 +26,11 @@ export function History() {
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
                 <td>{cycle.minutesAmount} minutos</td>
-                <td>{cycle.startDate.toISOString()}</td>
+                <td>{formatDistanceToNow(cycle.startDate, {
+                  addSuffix: true,
+                  locale: ptBR,
+                })
+                }</td>
                 <td>
                   {cycle.finishedDate && (
                     <Status statusColor="green">Concluído</Status>
