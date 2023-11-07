4bade40e33f7194c4bf4acce020351ae7f4ef7f1
walacefex <walacefex01@gmail.com>
2023-11-07 00:01:49 -0300

    using library immer

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -30,27 +30,26 @@ export function cycleReducer (state: CyclesState, action: any) {
         if (currentCycleIndex < 0) {
           return state;
         }
-        
+
         return produce(state, draft => {
           draft.activeCycleId = null;
           draft.cycles[currentCycleIndex].interruptedDate = new Date();
         })
       }
-      case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-         return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if(cycle.id === state.activeCycleId) {
-              return {
-                ...cycle,
-                finishedDate: new Date(),
-              }
-            }else{
-              return cycle  
-            }
-          }),
-          activeCycleId: null,
+      case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:{
+        const currentCycleIndex = state.cycles.findIndex((cycle) => {
+          return cycle.id === state.activeCycleId
+        });
+
+        if (currentCycleIndex < 0) {
+          return state;
         }
+        
+        return produce(state, draft => {
+          draft.activeCycleId = null;
+          draft.cycles[currentCycleIndex].finishedDate = new Date();
+        })
+      }
       default:
         return state
   }
